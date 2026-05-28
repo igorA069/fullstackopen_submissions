fb356d5b34b48b0f367a396e0b979ea73cee6227
Igor Achieser <igor.achieser@aumovio.com>
2026-05-28 15:56:42 +0200

    Provide "key" parameter

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,6 @@
 const Header = (props) => <h2>{props.courseName}</h2>
 
-const Content = (props) => props.parts.map(part => <Part part={part} />)
+const Content = (props) => props.parts.map(part => <Part key={part.id} part={part} />)
 
 const Part = (props) => <p>{props.part.name} {props.part.exercises}</p>
 
