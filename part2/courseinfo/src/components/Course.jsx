03a133d0d95f524bc0c14f8f2c6fe6c214aee141
Igor Achieser <igor.achieser@aumovio.com>
2026-05-28 15:51:45 +0200

    Submit part2 exercises 2.1-2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,15 @@
+const Header = (props) => <h2>{props.courseName}</h2>
+
+const Content = (props) => props.parts.map(part => <Part part={part} />)
+
+const Part = (props) => <p>{props.part.name} {props.part.exercises}</p>
+
+const Total = (props) => <b>Total of {props.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)} exercises</b>
+
+export const Course = (props) => (
+  <div>
+    <Header courseName={props.course.name} />
+    <Content parts={props.course.parts} />
+    <Total parts={props.course.parts} />
+  </div>
+)
