797c3baf278f7e67f956ee100e4f8aa39427c1ac
Igor Achieser <igor.achieser@aumovio.com>
2026-06-05 15:46:49 +0200

    Add ex 3.8: Configure output to also display the request content

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,8 @@ const app = express()
 app.use(express.json())
 
 const morgan = require('morgan')
-app.use(morgan('tiny'))
+morgan.token('json-content', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json-content'))
 
 let persons = [
     { 
