8992851e9403b4a8f6111341f01d2c587c834aeb
Igor Achieser <igor.achieser@aumovio.com>
2026-06-07 10:31:27 +0200

    Allow static serving; add command for building frontend and adding it to backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 const morgan = require('morgan')
 morgan.token('json-content', (req, res) => JSON.stringify(req.body))
