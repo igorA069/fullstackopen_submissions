4509d0c58ecde7eff6912bbc8a5e257743af4631
Igor Achieser <igor.achieser@aumovio.com>
2026-06-12 16:01:57 +0200

    Ex 4.5: add favoriteBlog + UTs

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -46,5 +46,64 @@ describe('totalLikes tests', () => {
         ]
         assert.strictEqual(listHelper.totalLikes(blogs), likesCount1 + likesCount2)
     })
+})
+
+describe('favoriteBlog tests', () => {
+    test('favoriteBlog of empty array is null', () => {
+        const blogs = []
+        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), null)
+    })
+
+    test('favoriteBlog of an array with 1 element equals that element', () => {
+        const likesCount = 13
+        const blogs = [
+            new Blog({
+                title: "some title",
+                author: "some author",
+                url: "www.example.com",
+                likes: likesCount
+            })]
+        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), blogs[0])
+    })
+
+    test('favoriteBlog of an array with multiple elements equals the blog with the most likes', () => {
+        const likesCount1 = 190
+        const likesCount2 = 1301
+        const blogs = [
+            new Blog({
+                title: "some title",
+                author: "some author",
+                url: "www.example.com",
+                likes: likesCount1
+            }),
+            new Blog({
+                title: "another title",
+                author: "another author",
+                url: "www.helloworld.com",
+                likes: likesCount2
+            })
+        ]
+        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), blogs[1])
+    })
+
+    test('favoriteBlog of an array with multiple elements equals the blog with the most likes', () => {
+        const likesCount1 = 190
+        const likesCount2 = 1301
+        const blogs = [
+            new Blog({
+                title: "another title",
+                author: "another author",
+                url: "www.helloworld.com",
+                likes: likesCount2
+            }),
+            new Blog({
+                title: "some title",
+                author: "some author",
+                url: "www.example.com",
+                likes: likesCount1
+            })
+        ]
+        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), blogs[0])
+    })
 
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,4 +4,16 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((acc, curr) => acc + curr.likes, 0)
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    let result = null
+    let mostLikes = -1
+    blogs.forEach(blog => {
+        if (blog.likes > mostLikes) {
+            mostLikes = blog.likes 
+            result = blog
+        }
+    })
+    return result
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
