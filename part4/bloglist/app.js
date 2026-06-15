e255dd7a3084458336315967be4ba95eca410fd5
Igor Achieser <igor.achieser@aumovio.com>
2026-06-15 19:30:42 +0200

    Move connection to db from index.js to app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const blogRouter = require('./controllers/blogs')
+const config = require('./config/config')
 
 const app = express()
 app.use(express.json())
 app.use('/api', blogRouter)
+
+mongoose.connect(config.MONGODB_CONNECT_STRING, { family: 4 })
 
 module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,9 +1,5 @@
-const mongoose = require('mongoose')
-
 const app = require('./app')
 const config = require('./config/config')
-
-mongoose.connect(config.MONGODB_CONNECT_STRING, { family: 4 })
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
