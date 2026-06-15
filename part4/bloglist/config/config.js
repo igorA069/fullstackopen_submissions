0e39e57722a5487ae41b32c4fac6c7318902cf01
Igor Achieser <igor.achieser@aumovio.com>
2026-06-15 19:33:23 +0200

    Use different env vars for testing. Install supertest

--- part4/bloglist/config/config.js
+++ part4/bloglist/config/config.js
@@ -1,5 +1,7 @@
 const PORT = 3003
 
-const MONGODB_CONNECT_STRING = process.env.MONGODB_CONNECT_STRING
+const NODE_ENV = process.env.NODE_ENV
+
+const MONGODB_CONNECT_STRING = NODE_ENV === 'test' ? process.env.MONGODB_TEST_CONNECT_STRING : process.env.MONGODB_CONNECT_STRING
 
 module.exports = { PORT, MONGODB_CONNECT_STRING }
