26d02d50a5ca5e0908f48cd0d8e601f2f16dc816
Igor Achieser <igor.achieser@aumovio.com>
2026-06-30 16:07:36 +0200

    Ex 5.7: collapsible blog details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isCollapsed, setCollapsed] = useState(true)
+
+  const style = {
+    borderStyle: 'none',
+    borderRadius: '5px',
+    background: 'lightgrey',
+    padding: '5px',
+    margin: '5px'
+  }
+
+  const toggleCollapsed = () => setCollapsed(!isCollapsed)
+
+  return (
+    <div style={style}>
+      {blog.title} {blog.author} <button onClick={ toggleCollapsed }>{ isCollapsed ? 'view' : 'hide' }</button>
+      {!isCollapsed && (
+        <div>
+          <br/>
+          {blog.url}<br/>
+          likes {blog.likes} <button>like</button><br/>
+          {blog.user.name}
+        </div>
+      )}
+    </div>  
+  )
+}
 
 export default Blog
