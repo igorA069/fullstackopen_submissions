85bfa29cffb635abc07f02751387e28a1eb61406
Igor Achieser <igor.achieser@aumovio.com>
2026-07-09 16:36:47 +0200

    Ex 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,27 +1,29 @@
 import { render, screen } from '@testing-library/react'
 import { test, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
 import Blog from './Blog'
 
+// instantiate a blog
+const blog = {
+  title: 'Title',
+  author:'Author',
+  url:'URL',
+  likes: 42,
+  user: { name: 'Name' }
+}
+
 test('title and author must be rendered, url and likes must not', () => {
-  // instantiate a blog
-  const blog = {
-    title: 'Title',
-    author:'Author',
-    url:'URL',
-    likes: 42,
-    user: { name: 'Name' }
-  }
   render(<Blog blog={blog}/>)
 
   // investigate the blog:
   let element
   // title and author must be rendered
   element = screen.getByText(blog.title, { exact:false })
-  expect(element).toBeDefined()
+  expect(element).toBeVisible()
 
   element = screen.getByText(blog.author, { exact:false })
-  expect(element).toBeDefined()
+  expect(element).toBeVisible()
 
   // url and likes must not be visible
   element = screen.queryByText(blog.url, { exact:false })
@@ -29,4 +31,19 @@ test('title and author must be rendered, url and likes must not', () => {
 
   element = screen.queryByText(blog.likes.toString(), { exact:false })
   expect(element).toBeNull()
+})
+
+test('url and likes become visible when the view button is clicked', async () => {
+  render(<Blog blog={blog}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  let element
+  element = screen.queryByText(blog.url, { exact:false })
+  expect(element).toBeVisible()
+
+  element = screen.queryByText(blog.likes.toString(), { exact:false })
+  expect(element).toBeVisible()
 })
