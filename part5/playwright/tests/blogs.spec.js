60e22e652e3d041446ee50ab92a25643e98a7e67
Igor Achieser <igor.achieser@aumovio.com>
2026-07-11 18:40:06 +0200

    Ex 5.18: add tests

--- part5/playwright/tests/blogs.spec.js
+++ part5/playwright/tests/blogs.spec.js
@@ -1,7 +1,17 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+        // empty db
+        await request.post('http://localhost:3003/testing/reset')
+        // create a user
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                username: 'user1',
+                name: 'user1name',
+                password: 'user1password'
+            }
+        })
         await page.goto('http://localhost:5173/')
     })
 
@@ -9,5 +19,28 @@ describe('Blog app', () => {
         await expect(page.getByText('log in to application')).toBeVisible()
         await expect(page.getByRole('textbox', { name: 'username' })).toBeVisible()
         await expect(page.getByRole('textbox', { name: 'password' })).toBeVisible()
+    })
+
+    describe('Login', () => {
+        test('Success with correct credentials', async ({ page }) => {
+            await page.getByRole('textbox', {name: 'user'}).fill('user1')
+            await page.getByRole('textbox', {name: 'password'}).fill('user1password')
+            await page.getByRole('button', {name: 'login'}).click()
+            await expect(page.getByText('user1 logged in')).toBeVisible()
+        })
+
+        test('Failure with existing user but wrong password', async ({ page }) => {
+            await page.getByRole('textbox', {name: 'user'}).fill('user1')
+            await page.getByRole('textbox', {name: 'password'}).fill('1234')
+            await page.getByRole('button', {name: 'login'}).click()
+            await expect(page.getByText('user1 logged in')).not.toBeVisible()
+        })
+
+        test('Failure with unknown user', async ({ page }) => {
+            await page.getByRole('textbox', {name: 'user'}).fill('unknown')
+            await page.getByRole('textbox', {name: 'password'}).fill('1234')
+            await page.getByRole('button', {name: 'login'}).click()
+            await expect(page.getByText('user1 logged in')).not.toBeVisible()
+        })
     })
 })
