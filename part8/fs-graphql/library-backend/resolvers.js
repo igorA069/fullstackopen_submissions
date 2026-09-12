5194dff912984de6bc901279f690618e1622a923
Igor Achieser <igor.achieser@aumovio.com>
2026-09-12 12:58:13 +0200

    Switch allBooks, allAuthors, editAuthor to mongoose. Remove hardcoded authors and books.

--- part8/fs-graphql/library-backend/resolvers.js
+++ part8/fs-graphql/library-backend/resolvers.js
@@ -8,128 +8,25 @@ const {
   getAuthorByName,
 } = require("./utils");
 
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
- */
-
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "Demons",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
-
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
+    bookCount: async () => await Book.countDocuments({}),
 
-    authorCount: () => authors.length,
+    authorCount: async () => await Author.countDocuments({}),
 
-    allBooks: (root, args) => {
-      const booksFilteredByAuthor = !args.author
-        ? books
-        : books.filter((book) => book.author === args.author);
-      const booksFilteredByGenre = !args.genre
-        ? booksFilteredByAuthor
-        : booksFilteredByAuthor.filter((book) =>
-            book.genres.includes(args.genre),
-          );
-      return booksFilteredByGenre.map((book) => ({
-        ...book,
-        author: {
-          // return an object instead of just the name string
-          ...getAuthorByName(authors, book.author),
-          bookCount: getAuthorBookCount(books, book.author),
-        },
-      }));
+    allBooks: async (root, args) => {
+      const searchCriteria = {};
+      if (args.author) {
+        // todo
+      }
+      if (args.genre) {
+        // todo
+      }
+      const books = await Book.find(searchCriteria).populate("author");
+      return books;
     },
 
-    allAuthors: () =>
-      authors.map((author) => ({
-        ...author,
-        bookCount: books.filter((book) => book.author === author.name).length,
-      })),
+    allAuthors: async () => await Author.find({}),
   },
 
   Mutation: {
@@ -158,21 +55,14 @@ const resolvers = {
       return newBook;
     },
 
-    editAuthor: (root, args) => {
-      if (!isExistingAuthor(args.name, authors)) {
+    editAuthor: async (root, args) => {
+      const authorToUpdate = await Author.findOne({ name: args.name });
+      if (!authorToUpdate) {
         return null;
       }
-      const authorToUpdate = authors.find(
-        (author) => author.name === args.name,
-      );
       authorToUpdate.born = args.setBornTo;
-      authors = authors.map((author) =>
-        author.name !== args.name ? author : authorToUpdate,
-      );
-      return {
-        ...authorToUpdate,
-        bookCount: getAuthorBookCount(books, authorToUpdate.name),
-      };
+      await authorToUpdate.save();
+      return authorToUpdate;
     },
   },
 };
