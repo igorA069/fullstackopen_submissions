2c70d8b064131a78b41d4fd44f792745303ff6a9
Igor Achieser <igor.achieser@aumovio.com>
2026-09-15 19:48:07 +0200

    Set and handle NODE_ENV (dev / test)

--- part8/fs-graphql/library-backend/index.js
+++ part8/fs-graphql/library-backend/index.js
@@ -6,7 +6,10 @@ const startServer = require("./server");
 const BACKEND_PORT = process.env.BACKEND_PORT || 4000;
 
 // TODO: use different connect strings for different configurations (dev, test, prod):
-const MONGODB_CONNECT_STRING = process.env.MONGODB_DEV_CONNECT_STRING;
+const MONGODB_CONNECT_STRING =
+  process.env.NODE_ENV === "dev"
+    ? process.env.MONGODB_DEV_CONNECT_STRING
+    : process.env.MONGODB_TEST_CONNECT_STRING;
 
 const main = async () => {
   await connectToDb(MONGODB_CONNECT_STRING);
